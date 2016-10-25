1af9080d640093214a53d96ba1838d10aa4e9dbe
Sarup Banskota <sbanskota08@gmail.com>
2016-10-25 20:27:48 +0300

    Embed typeform within page

---
+++ startup/common-files/js/typeform.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#typeform-container').hide();
+  $('#get-started').on('click', function() {
+    $('#information-container').hide();
+    $('#typeform-container').show();
+  })
+});
